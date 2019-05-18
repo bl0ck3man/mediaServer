6e909d47ebddbfc77d5e2db30f9988b04e1d5f8c
Sergey White <srsergey.ivanenko@gmail.com>
2019-05-18 13:57:02 +0300

    afp.conf

--- images/netatalk/docker-entrypoint.sh
+++ images/netatalk/docker-entrypoint.sh
@@ -35,11 +35,6 @@ echo ---end---afp.conf--
 mkdir -p /var/run/dbus
 rm -f /var/run/dbus/pid
 service dbus start
-if [ "${AVAHI}" == "1" ]; then
-    sed -i '/rlimit-nproc/d' /etc/avahi/avahi-daemon.conf
-    avahi-daemon -D
-else
-    echo "Skipping avahi daemon, enable with env variable AVAHI=1"
-fi;
+avahi-daemon -D
 
 exec netatalk -d
