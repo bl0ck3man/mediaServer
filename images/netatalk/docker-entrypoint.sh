39557056caf0181f16d0e8baee5f174ac8797b6e
Sergey White <srsergey.ivanenko@gmail.com>
2019-05-18 13:55:19 +0300

    afp.conf

--- images/netatalk/docker-entrypoint.sh
+++ images/netatalk/docker-entrypoint.sh
@@ -34,7 +34,7 @@ echo ---end---afp.conf--
 
 mkdir -p /var/run/dbus
 rm -f /var/run/dbus/pid
-dbus-daemon --system
+service dbus start
 if [ "${AVAHI}" == "1" ]; then
     sed -i '/rlimit-nproc/d' /etc/avahi/avahi-daemon.conf
     avahi-daemon -D
